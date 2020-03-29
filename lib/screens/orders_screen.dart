b29b7e6f07b3d352e2e84a0a5611ee2af34463c4
Alsoxavi <oscarj9410@gmail.com>
2020-03-29 12:32:35 -0500

    Using Future Builder on orders screen.

--- lib/screens/orders_screen.dart
+++ lib/screens/orders_screen.dart
@@ -5,49 +5,55 @@ import '../providers/orders_provider.dart';
 import '../widgets/order_screen_item.dart';
 import '../widgets/app_drawer.dart';
 
-class OrdersScreen extends StatefulWidget {
-  static const routeName = '/orders';
-
-  @override
-  _OrdersScreenState createState() => _OrdersScreenState();
-}
-
-class _OrdersScreenState extends State<OrdersScreen> {
-  var _isLoading = false;
-
-  @override
-  void initState() {
-    _isLoading = true;
-    Provider.of<OrdersProvider>(context, listen: false).fetchOrders().then((_) {
-      setState(() {
-        _isLoading = false;
-      });
-    });
-
-    super.initState();
-  }
+class OrdersScreen extends StatelessWidget {
+  static const routeName = '/orders';  
 
   @override
   Widget build(BuildContext context) {
-    final orderData = Provider.of<OrdersProvider>(context);
-
     return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          'Your Orders',
+        appBar: AppBar(
+          title: Text(
+            'Your Orders',
+          ),
         ),
-      ),
-      drawer: AppDrawer(),
-      body: _isLoading
-          ? Center(
-              child: CircularProgressIndicator(),
-            )
-          : ListView.builder(
-              itemBuilder: (ctx, i) => OrderScreenItem(
-                orderData.orders[i],
-              ),
-              itemCount: orderData.orders.length,
-            ),
-    );
+        drawer: AppDrawer(),
+        body: FutureBuilder(
+          future: Provider.of<OrdersProvider>(context, listen: false).fetchOrders(),
+          builder: (ctx, dataSnapshot) {
+            if (dataSnapshot.connectionState == ConnectionState.waiting) {
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            } else {
+              if (dataSnapshot.error != null) {
+                // Error handling logic
+                return Center(
+                  child: Text('An error ocurred!'),
+                );
+              } else {
+                return Consumer<OrdersProvider>(
+                  builder: (ctx, orderData, child) => ListView.builder(
+                    itemBuilder: (ctx, i) => OrderScreenItem(
+                      orderData.orders[i],
+                    ),
+                    itemCount: orderData.orders.length,
+                  ),
+                );
+              }
+            }
+          },
+        ));
   }
 }
+
+// @override //REEMPLAZADO POR FUTURE BUILDER
+  // void initState() {
+  //   _isLoading = true;
+  //   Provider.of<OrdersProvider>(context, listen: false).fetchOrders().then((_) {
+  //     setState(() {
+  //       _isLoading = false;
+  //     });
+  //   });
+
+  //   super.initState();
+  // }
